02bd87e43ed466ce92f2e741070fa724fe157469
Noralf Trønnes <noralf@tronnes.org>
2020-05-06 19:09:05 +0200

    drm/client: Add drm_client_modeset_disable()
    Add a way for clients to disable all outputs.
    
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- include/drm/drm_client.h
+++ include/drm/drm_client.h
@@ -217,6 +217,7 @@ int drm_client_modeset_commit_locked(struct drm_client_dev *client);
 int drm_client_modeset_commit(struct drm_client_dev *client);
 int drm_client_modeset_dpms(struct drm_client_dev *client, int mode);
 int drm_client_modeset_wait_for_vblank(struct drm_client_dev *client, unsigned int crtc_index);
+int drm_client_modeset_disable(struct drm_client_dev *client);
 
 /**
  * drm_client_for_each_modeset() - Iterate over client modesets
