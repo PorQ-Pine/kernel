7c683be697f2c8624e0d86933e3db003be434ffb
hrdl <31923882+hrdl-github@users.noreply.github.com>
2025-01-27 00:24:00 +0100

    rockchip_ebc_blit_neon: remove unused function

--- drivers/gpu/drm/rockchip/rockchip_ebc_blit_neon.c
+++ drivers/gpu/drm/rockchip/rockchip_ebc_blit_neon.c
@@ -11,11 +11,4 @@
 
 #include "rockchip_ebc_blit_neon.h"
 
-void rockchip_ebc_blit_pixels_blocks_neon(u8 *dst_line, const u8 *src_line,
-					  int byte_width, int pitch,
-					  int clip_height)
-{
-}
-EXPORT_SYMBOL(rockchip_ebc_blit_pixels_blocks_neon);
-
 MODULE_LICENSE("GPL v2");
