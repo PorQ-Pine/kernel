d3860ce1b6285df0fa7262cbe81bb013304bfa0a
hrdl <31923882+hrdl-github@users.noreply.github.com>
2025-02-12 17:32:41 +0100

    rockchip_ebc: add experimental option to shorten a2 waveforms in use_neon & 4 mode

--- drivers/gpu/drm/rockchip/rockchip_ebc_blit_neon.h
+++ drivers/gpu/drm/rockchip/rockchip_ebc_blit_neon.h
@@ -33,6 +33,7 @@ void rockchip_ebc_update_blit_fnum_prev_neon(const struct rockchip_ebc_ctx *ctx,
 void rockchip_ebc_blit_direct_fnum_a2_neon(const struct rockchip_ebc_ctx *ctx,
 					u8 *phase, u8 *frame_num, u8 *next,
 					u8 *prev, const struct drm_epd_lut *lut,
+					u8 a2_shorten_waveform,
 					const struct drm_rect *clip);
 
 void rockchip_ebc_blit_frame_num_neon(const struct rockchip_ebc_ctx *ctx,
