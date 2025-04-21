8ea182243a623ee549c4e6971be77df7ab678605
hrdl <31923882+hrdl-github@users.noreply.github.com>
2025-04-22 01:34:43 +0200

    [tps65185] fix write_event_config signature

--- drivers/regulator/tps65185-regulator.c
+++ drivers/regulator/tps65185-regulator.c
@@ -328,7 +328,7 @@ static int tps65185_read_event_config(struct iio_dev *indio_dev,
 static int tps65185_write_event_config(struct iio_dev *indio_dev,
 				       const struct iio_chan_spec *chan,
 				       enum iio_event_type type,
-				       enum iio_event_direction dir, int state)
+				       enum iio_event_direction dir, bool state)
 {
 	struct tps65185 *tps = iio_device_get_drvdata(indio_dev);
 	u8 mask = TPS65185_INT1_DTX;
