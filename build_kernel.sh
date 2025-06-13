14e7524dacebfa8f035b80cea0795c1cf2958ebf
Nicolas Mailloux <nicolecrivain@gmail.com>
2025-06-13 16:24:37 -0400

    Some changes

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 [ -z "${THREADS}" ] && THREADS=1
-rm -f initrd_base/.rooted
-[ ${ROOT} == 1 ] && touch initrd_base/.rooted
 git rev-parse --short HEAD > initrd_base/.commit
 rm -rf initrd_base/lib
 make distclean
